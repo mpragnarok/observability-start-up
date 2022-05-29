9277eff93d6215193ed868f51d20b40b45150f7a
minahuang <minahuang@jubo.health>
2022-05-29 18:25:50 +0800

    feat(init): init project

---
+++ node-metric-startup/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const prom = require("prom-client");
+const responseTime = require("response-time");
+const Registry = prom.Registry;
+const register = new Registry();
+// register prometheus metrics
+prom.collectDefaultMetrics({ register });
+
+const httpRequestDuration = new prom.Histogram({
+  name: "http_request_duration_seconds",
+  help: "The http request duration in seconds",
+  buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10],
+  labelNames: ["route", "method", "statusCode"],
+});
+register.registerMetric(httpRequestDuration);
+app.use(
+  responseTime(function (req, res, time) {
+    const { statusCode } = res;
+    const { method, originalUrl } = req;
+    const duration = Math.round(time) / 1000;
+    httpRequestDuration.observe({ method, route: originalUrl, statusCode }, duration);
+  }),
+);
+app.get("/", (req, res) =>
+  res.json({
+    "GET /": "All Routes",
+    "GET /hello": "{foo:bar}",
+    "GET /metrics": "Metrics data",
+  }),
+);
+
+// Setup server to Prometheus scrapes:
+app.get("/metrics", async (req, res) => {
+  try {
+    res.set("Content-Type", register.contentType);
+    res.end(await register.metrics());
+  } catch (err) {
+    res.status(500).end(err);
+  }
+});
+// hello world rest endpoint
+app.get("/foo", (req, res) => res.json({ foo: "bar" }));
+
+app.listen(8080, function () {
+  console.log("Listening at http://localhost:8080");
+});
